4cee42761fec7889b17f989fe09ffea09fb44f71
Moshikot3 <moshikot015@gmail.com>
2023-07-09 03:01:08 +0300

    אקדא

--- helpers/users_helper.js
+++ helpers/users_helper.js
@@ -1,6 +1,6 @@
 async function isAdmin(msg) {
     const author = await msg.getContact();
-    if(author.number == "972544911249" || author.number == "972539726337")
+    if(author.number == "972544911249")
         return true;
     return false;
 }
