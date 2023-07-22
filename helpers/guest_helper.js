fea00bf21ac3bd4c9032e52e11271283dc5b0c06
Moshikot3 <moshikot015@gmail.com>
2023-07-22 21:19:09 +0300

    more updates

--- helpers/guest_helper.js
+++ helpers/guest_helper.js
@@ -2,30 +2,36 @@ const users = require('./users_helper');
 const database = require("./db_helper");
 
 async function SendGuestMessage(client, msg) {
-    let guestmsg = ""
+  if (!await users.isAdmin(msg)) {
+    const isConfig = await database.read("config");
 
-    if(!(await database.read("config"))){
-        guestmsg = ""
-    }else{
-        guestmsg = (await database.read("config")).guestmsg;
+    let guestmsg = "";
+
+    if (!isConfig) {
+      guestmsg = "";
+    } else {
+      guestmsg = isConfig.guestmsg;
     }
-        if(guestmsg && guestmsg != ""){
 
+    if (guestmsg && guestmsg != "") {
+      console.log("Guest Message: " + guestmsg);
+      await msg.reply(guestmsg);
+    } else {
+      console.log("guest message set");
+    }
 
-
-        console.log("Guest Message: "+guestmsg);
-        await msg.reply(guestmsg);
-        }else{
-            console.log("guest message set");
-        }
-
-        let guestContact = await msg.getContact();
-        await msg.forward("972544911249@c.us");
-        //await client.sendMessage("@c.us", "*הודעה מהמשתמש "+abacontact.pushname+":*\n\n"+AbaMessage_String);
-        await client.sendMessage("972544911249@c.us", guestContact);
-
+    // OPT_GuestMSGToAdmin
+    console.log(isConfig.OPT_GuestMSGToAdmin);
+    if (isConfig.OPT_GuestMSGToAdmin && isConfig.SEC_AdminList != "") {
+      let guestContact = await msg.getContact();
+      const adminList = isConfig.SEC_AdminList.split(","); // Parse the admin numbers into an array
+      for (const admin of adminList) {
+        const adminNumber = `972${Number(admin).toString()}@c.us`; // Convert admin number to WhatsApp format
+        await msg.forward(adminNumber); // Forward the guest message to each admin
+        await client.sendMessage(adminNumber, guestContact); // Send the contact info to each admin
+      }
+    }
+  }
 };
-
-
 
 module.exports = { SendGuestMessage };
--- helpers/users_helper.js
+++ helpers/users_helper.js
@@ -1,17 +1,32 @@
-async function isAdmin(msg) {
+const database = require("./db_helper");
 
-    let admin = ["972544911249@c.us","972539726337@c.us"];
-    console.log("Admin check msg.from:")
-    console.log(msg.from);
-    const author =  msg.from
-    console.log(author);
-    if(admin.includes(author)){
-        return true;
-    }
-    return false;
+async function isAdmin(msg) {
+  const isConfig = await database.read("config");
+  console.log(isConfig.SEC_AdminList);
+
+  // Parse the SEC_AdminList string and convert it into an array of admin numbers
+  let adminList = isConfig.SEC_AdminList.split(",");
+
+  // Update each admin number to the WhatsApp format and remove leading zeros
+  adminList = adminList.map((admin) => {
+    const adminNumber = Number(admin).toString(); // Convert to number and back to string to remove leading zeros
+    return `972${adminNumber}@c.us`;
+  });
+
+  console.log("Admin check msg.from:");
+  console.log(msg.from);
+  const author = msg.from;
+  console.log(adminList);
+  
+  // Check if the message sender's number is in the adminList
+  if (adminList.includes(author)) {
+    console.log("Is Admin");
+    return true;
+  }
+  console.log("Is Not Admin");
+  return false;
 }
 
-
 module.exports = {
-    isAdmin,
+  isAdmin,
 };
