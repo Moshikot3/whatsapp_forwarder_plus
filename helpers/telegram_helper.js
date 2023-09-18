1e20e0fc77b9c88ade4fb84328ab881deadffb66
Moshe Tamayev <moshikot015@gmail.com>
2023-09-18 13:15:33 +0300

    Some beautifing

--- helpers/telegram_helper.js
+++ helpers/telegram_helper.js
@@ -15,7 +15,7 @@ async function ForwardTelegram(msg, isConfig, telquotemsg) {
     process.env.NTBA_FIX_350 = 0;
     let tlgrmsg;
 
-    try{
+    try {
         switch (msg.type) {
             case "chat":
                 var options = {
@@ -82,8 +82,8 @@ async function ForwardTelegram(msg, isConfig, telquotemsg) {
                     options.caption = modifiedText + signaturetxt;
                 }
                 //console.log(attachmentData);
-                tlgrmsg = await telegram.sendDocument(OPT_TelegramChannelChatID, Buffer.from(attachmentData.data , 'base64'), {caption: msg}, options);
-                
+                tlgrmsg = await telegram.sendDocument(OPT_TelegramChannelChatID, Buffer.from(attachmentData.data, 'base64'), { caption: msg }, options);
+
                 break;
             default:
                 break;
@@ -92,29 +92,28 @@ async function ForwardTelegram(msg, isConfig, telquotemsg) {
 
         }
         console.log("Message forwarded to Telegram");
-    }catch{
-         tlgrmsg = "";
+    } catch {
+        tlgrmsg = "";
         await msg.react("⚠️");
         await msg.reply("⚠️שים לב, ההודעה לא הועברה לטלגרם.⚠️\nעל מנת להפסיק הודעה זו יש לכבות העברה לטלגרם באמצעות הפורטל\n\nאם הנך מנסה להעביר הודעות לטלגרם, שים לב שהגדרת את כל ההגדרות כשורה.\nבמידה והבעיה עדיין נמשכת, יש לפנות למפתח.");
         console.log("MEssage did not forwarded to Telegram");
     }
-    
+
 
 };
 
 
-async function delMessage(chatid, msgid)
-{
+async function delMessage(chatid, msgid) {
     const isConfig = await database.read("config");
-    if(isConfig) {
-        console.log("Entering DeleteMessage"+chatid+" "+msgid);
+    if (isConfig) {
+        console.log("Entering DeleteMessage" + chatid + " " + msgid);
         const OPT_TelegramBotToken = isConfig.OPT_TelegramBotToken;
         const telegram = new TelegramBot(OPT_TelegramBotToken, { polling: false });
 
 
-        try{
+        try {
             await telegram.deleteMessage(chatid, msgid);
-        }catch{
+        } catch {
             await msg.react("⚠️");
             await msg.reply("⚠️בעיה במחיקת הודעה בטלגרם, יש לדווח למפתח⚠️");
         }
@@ -123,88 +122,86 @@ async function delMessage(chatid, msgid)
 
 };
 
-async function SendWAFPStatus (botStatus)
-{
+async function SendWAFPStatus(botStatus) {
 
-        const isConfig = await database.read("config");
-        
-        //I would like ot make sure that if there's no isConfig.OPT_TelegramBotToken the app will not crash
+    const isConfig = await database.read("config");
 
-        if (isConfig){
+    //I would like ot make sure that if there's no isConfig.OPT_TelegramBotToken the app will not crash
 
-            const OPT_TelegramBotToken = isConfig.OPT_TelegramBotToken;
-            const telegram = new TelegramBot(OPT_TelegramBotToken, { polling: false });
-            const OPT_TelegramAdminChatID = isConfig.OPT_TelegramAdminChatID;
-            // console.log(OPT_TelegramAdminChatID);
-            if(OPT_TelegramAdminChatID){
-                process.env.NTBA_FIX_319 = 1;
-                process.env.NTBA_FIX_350 = 0;
-        
-                var options = {
-                    parse_mode: 'Markdown'
-        
-                };
-                await telegram.sendMessage(OPT_TelegramAdminChatID, botStatus, options);
-            }else{
-                return;
-            }
-    
+    if (isConfig) {
 
+        const OPT_TelegramBotToken = isConfig.OPT_TelegramBotToken;
+        const telegram = new TelegramBot(OPT_TelegramBotToken, { polling: false });
+        const OPT_TelegramAdminChatID = isConfig.OPT_TelegramAdminChatID;
+        // console.log(OPT_TelegramAdminChatID);
+        if (OPT_TelegramAdminChatID) {
+            process.env.NTBA_FIX_319 = 1;
+            process.env.NTBA_FIX_350 = 0;
+
+            var options = {
+                parse_mode: 'Markdown'
+
+            };
+            await telegram.sendMessage(OPT_TelegramAdminChatID, botStatus, options);
+        } else {
+            return;
         }
+
+
+    }
 
 
 
 
 };
 
-async function Sendqrcode (qrcode)
-{
+async function Sendqrcode(qrcode) {
 
-        const isConfig = await database.read("config");
-        
-        //I would like ot make sure that if there's no isConfig.OPT_TelegramBotToken the app will not crash
+    const isConfig = await database.read("config");
 
-        if (isConfig){
+    //I would like ot make sure that if there's no isConfig.OPT_TelegramBotToken the app will not crash
 
-            const OPT_TelegramBotToken = isConfig.OPT_TelegramBotToken;
-            const telegram = new TelegramBot(OPT_TelegramBotToken, { polling: false });
-            const OPT_TelegramAdminChatID = isConfig.OPT_TelegramAdminChatID;
-            // const qr = Buffer.from(qrcode, 'base64');
-            // fs.writeFileSync('temp_qr.png', qr);
-            console.log(qrcode);
-            const base64Image = qrcode; // Your base64 image string here
+    if (isConfig) {
 
-// Extract the image data from the base64 string
-const matches = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
+        const OPT_TelegramBotToken = isConfig.OPT_TelegramBotToken;
+        const telegram = new TelegramBot(OPT_TelegramBotToken, { polling: false });
+        const OPT_TelegramAdminChatID = isConfig.OPT_TelegramAdminChatID;
+        // const qr = Buffer.from(qrcode, 'base64');
+        // fs.writeFileSync('temp_qr.png', qr);
+        console.log(qrcode);
+        const base64Image = qrcode; // Your base64 image string here
 
-if (matches.length !== 3) {
-  console.error('Invalid base64 image string');
-  process.exit(1);
-}
+        // Extract the image data from the base64 string
+        const matches = base64Image.match(/^data:([A-Za-z-+\/]+);base64,(.+)$/);
 
-const [, imageType, base64Data] = matches;
-
-// Create a buffer from the base64 data
-const buffer = Buffer.from(base64Data, 'base64');
-
-
-
-
-            if(OPT_TelegramAdminChatID){
-                process.env.NTBA_FIX_319 = 1;
-                process.env.NTBA_FIX_350 = 0;
-        
-                var options = {
-                    parse_mode: 'Markdown'
-        
-                };
-                await telegram.sendPhoto(OPT_TelegramAdminChatID, buffer, options);
-            }else{
-                return;
-            }
-    
-
+        if (matches.length !== 3) {
+            console.error('Invalid base64 image string');
+            process.exit(1);
         }
+
+        const [, imageType, base64Data] = matches;
+
+        // Create a buffer from the base64 data
+        const buffer = Buffer.from(base64Data, 'base64');
+
+
+
+
+        if (OPT_TelegramAdminChatID) {
+            process.env.NTBA_FIX_319 = 1;
+            process.env.NTBA_FIX_350 = 0;
+
+            var options = {
+                parse_mode: 'Markdown'
+
+            };
+            await telegram.sendPhoto(OPT_TelegramAdminChatID, buffer, options);
+        } else {
+            return;
+        }
+
+
+    }
 
 
 
